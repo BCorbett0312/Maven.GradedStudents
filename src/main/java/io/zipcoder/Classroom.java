f6fded0aba8bb6f312661a1a557c186110ba8589
bcorbett0312 <benjamin.t.corbett@gmail.com>
2019-06-05 20:26:55 -0400

    Student Class completed (2nd Commit)

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,83 @@
 package io.zipcoder;
 
+import java.util.*;
+
 public class Classroom {
+    ArrayList<Student> students;
+
+    public Classroom(ArrayList<Student> students) {
+        this.students = students;
+    }
+
+    public Classroom(int maxNumberOfStudents){
+        students = new ArrayList<>(maxNumberOfStudents);
+    }
+
+    public Classroom(){
+        students = new ArrayList<>(30);
+    }
+
+
+    public ArrayList<Student> getStudents() {
+        return this.students;
+    }
+
+    public Double getAverageExamScore(){
+        Double sumOfAllScores=0.0;
+        for(Student each: students){
+            sumOfAllScores+= each.getAverageExamScore();
+        }
+        return sumOfAllScores/students.size();
+    }
+
+    public void addStudent(Student student){
+        students.add(student);
+
+//        for(int i = 0; i < students.length;i++){
+//            if(students == null){
+//                students[i] = student;
+//                break;
+//            }
+//
+//        }
+
+    }
+
+    public void removeStudent(String firstName, String lastName){
+        for(Student each: students){
+            if(firstName.equals(each.getFirstName()) && lastName.equals(each.getLastName())) {
+                students.remove(each);
+            }
+
+        }
+
+    }
+
+    public ArrayList<Student> getStudentsByScore(){
+        Comparator<Student> avgTestScoreComparator = Comparator.comparing(Student::getAverageExamScore).thenComparing(Student::getLastName);
+
+
+
+        //Comparator<Student> nameComp = Comparator.comparing(Student::getLastName);
+
+
+        //Arrays.sort(students, avgTestScoreComparator);
+        //Arrays.sort(students, nameComp);
+
+//
+//        for (int i = 1; i < students.length-1; i++){
+//            if (students[i].getAverageExamScore() > students[i-1].getAverageExamScore()){
+//                Student temp = students[i-1];
+//                students[i-1] = students[i];
+//                students[i]= temp;
+//                i =1 ;
+//            }
+//        }
+        System.out.println(students);
+        return students;
+    }
+
+
+
+
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,56 @@
 package io.zipcoder;
 
+import com.sun.deploy.security.MozillaMyKeyStore;
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class ClassroomTest {
+    @Test
+    public void getStudents() {
+    }
+
+    @Test
+    public void getAverageExamScore() {
+    }
+
+    @Test
+    public void addStudent() {
+    }
+
+    @Test
+    public void removeStudent() {
+    }
+
+    @Test
+    public void getStudentsByScore() {
+        Classroom myClassRoom = new Classroom();
+
+        ArrayList<Double> scores1 = new ArrayList<>();
+        ArrayList<Double> scores2 = new ArrayList<>();
+        ArrayList<Double> scores3 = new ArrayList<>();
+
+        Student student1 = new Student ("Charles", "last", scores1);
+        Student student2 = new Student ("Clyde", "heidi", scores2);
+        Student student3 = new Student ("MozillaMyKeyStore", "big", scores2);
+        Student student4 = new Student ("Earl", "hugh", scores3);
+        scores1.add(98.0);
+        scores1.add(93.0);
+        scores2.add(90.0);
+        scores3.add(55.0);
+
+        myClassRoom.addStudent(student1);
+        myClassRoom.addStudent(student2);
+        myClassRoom.addStudent(student3);
+        myClassRoom.addStudent(student4);
+
+
+        //System.out.println(myClassRoom.getStudents());
+        //myClassRoom.getStudents();
+        myClassRoom.getStudentsByScore();
+
+
+
+    }
 }
